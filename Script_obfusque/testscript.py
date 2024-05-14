3a0c13d28cf211a534b7fdb4c306144fe69106b0
arnaud <arnaudcoubran@gmail.com>
2024-05-14 14:24:52 +0200

    Create testscript.py

---
+++ Script_obfusque/testscript.py
@@ -0,0 +1,1 @@
+exec(__import__('base64').b64decode('IyBGb25jdGlvbiBkdSBzY3JpcHQgZGUgdGVzdCAKCmRlZiBsb2dpbih1c2VybmFtZSwgcGFzc3dvcmQpOgogICAgaWYgdXNlcm5hbWUgPT0gImFkbWluIiBhbmQgcGFzc3dvcmQgPT0gInNlY3JldCI6CiAgICAgICAgcmV0dXJuIFRydWUKICAgIGVsc2U6CiAgICAgICAgcmV0dXJuIEZhbHNlCiAgICA=').decode('utf-8'))
